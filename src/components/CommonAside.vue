3b39eeebe1d7105a44c2c2adf94b4f555f9cfc16
ChinesePeach <sunctao@qq.com>
2022-07-01 17:17:15 +0800

    upp

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -11,7 +11,7 @@
                 </el-icon>
                 <span>仪表盘</span>
             </el-menu-item>
-            <el-menu-item index="/admin/user">
+            <el-menu-item index="/admin/user" :disabled="disable">
                 <el-icon>
                     <UserFilled />
                 </el-icon>
@@ -43,7 +43,8 @@
 export default {
     data() {
         return {
-            defaultActive: undefined
+            defaultActive: undefined,
+            disable: true
         }
     },
     methods: {
@@ -52,6 +53,9 @@ export default {
 
         setCurrentRoute() {
             this.defaultActive = this.$route.path //通过他就可以监听到当前路由状态并激活当前菜单
+        },
+        getUserPower() {
+            this.disable = (localStorage.getItem('power') === "Super Admin" ? false : true)
         }
     },
     watch: {
@@ -61,6 +65,7 @@ export default {
     },
     created() {
         this.setCurrentRoute()
+        this.getUserPower()
     }
 
 }
